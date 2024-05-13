5be3d29aef704f5a4a1a201cf07c65f7313a1f40
ahmedelgebali <elgebalia34@gmail.com>
2024-05-13 13:12:28 +0300

    complating the code

--- src/demo.java
+++ src/demo.java
@@ -1,6 +1,4 @@
-import java.sql.Connection;
-import java.sql.DriverManager;
-import java.sql.SQLException;
+import java.sql.*;
 
 public class demo{
     public static void main(String[] args) {
@@ -8,9 +6,17 @@ public class demo{
         String uname = "postgres";
         String url = "jdbc:postgresql://localhost:5432/postgres";
         String pass = "ahmed@jdbc";
+        String query = "SELECT * FROM public.cars";
 
         try {
             Connection con = DriverManager.getConnection(url,uname,pass);
+
+            Statement st = con.createStatement();
+            ResultSet rs = st.executeQuery(query);
+            rs.next();
+            String qResult = rs.getString(1);
+            System.out.println(qResult);
+
 
 
         }catch (SQLException e){
